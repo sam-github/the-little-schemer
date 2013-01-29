b83741729a4ff94d63960eacc58d61335e6f94e3
Sam Roberts <vieuxtech@gmail.com>
2013-01-28 20:50:54 -0800

    Notes on other ways to define first/rest, and other scheme primitives.

--- src/the_little_schemer_in_clojure/core.clj
+++ src/the_little_schemer_in_clojure/core.clj
@@ -15,8 +15,17 @@
 ; First is defined over collections, thefore it is undefined over not collections
 (defn atom? [a] (not (coll? a)))
 
+; http://juliangamble.com/blog/2012/07/20/the-little-schemer-in-clojure-chapter-1/
+; Suggests using seq? instead of coll?, and then points to this, which I don't understand:
+;  http://clojure.org/lazy
+
+; http://hyperpolyglot.org/lisp
+;   says cljr has list?, so atom? would be not list?
+
 ; A collection is null? when the rest is nil
 (defn null? [l] (nil? (next l)))
+
+; cljr has empty?
 
 ; Identicality is called eq? in scheme, identical? in clojure
 (def eq? identical?)
