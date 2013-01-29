a1e2a190dd639974af5bd349bdccc2981a3f493a
Sam Roberts <vieuxtech@gmail.com>
2013-01-28 17:13:23 -0800

    clojure equivalents of car, cdr, atom?, null?, eq?

--- src/the_little_schemer_in_clojure/core.clj
+++ src/the_little_schemer_in_clojure/core.clj
@@ -5,3 +5,23 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+; The scheme primitives
+
+; car/cdr are first/rest
+(def car first)
+(def cdr rest)
+
+; First is defined over collections, thefore it is undefined over not collections
+(defn atom? [a] (not (coll? a)))
+
+; A collection is null? when the rest is nil
+(defn null? [l] (nil? (next l)))
+
+; Identicality is called eq? in scheme, identical? in clojure
+(def eq? identical?)
+
+; lambda -> fn
+; #t -> true
+; #f -> false
+
--- test/the_little_schemer_in_clojure/core_test.clj
+++ test/the_little_schemer_in_clojure/core_test.clj
@@ -2,6 +2,31 @@
   (:use clojure.test
         the-little-schemer-in-clojure.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest car-test
+  (testing "car"
+    (is (= 'a (car '(a b))))))
+    
+(deftest cdr-test
+  (testing "cdr"
+    (is (= '(b c) (cdr '(a b c))))))
+    
+(deftest atom-test
+  (testing "atom?"
+    (is (= true (atom? 1)))))
+
+(deftest not-atom-test
+  (testing "not atom?"
+    (is (= false (atom? '(1 2))))))
+
+(deftest null-test
+  (testing "null?"
+    (is (= true (null? '())))))
+
+(deftest not-null-test
+  (testing "not null?"
+    (is (= false (null? '(1 2))))))
+
+    
+    
+    
+    
