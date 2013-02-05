e355d73f4768233377e471fb2bb220247bebf932
Sam Roberts <vieuxtech@gmail.com>
2013-02-04 21:58:22 -0800

    Wrote my own collector function, to test if I remember.

--- src/the_little_schemer_in_clojure/core.clj
+++ src/the_little_schemer_in_clojure/core.clj
@@ -29,8 +29,14 @@
 (def eq? identical?)
 
 ; lambda -> fn
+; I cribbed this from stackoverflow, and don't understand its syntax!
+; Better to use fn.
+;(defmacro lambda [args & body] `(fn ~(vec args) ~@body))
+
 ; #t -> true
 ; #f -> false
+; cond/else -> cond/:else, but cond is a bit different in clojure
+
 
 ; Chapter 8
 
@@ -39,4 +45,23 @@
    (eq? (car lat) a) (multirember&co a (cdr lat) (fn [newlat seen] (col newlat (cons (car lat) seen))))
    :else (multirember&co a (cdr lat) (fn [newlat seen] (col (cons (car lat) newlat) seen))))))
 
+
+
+; Partition a list into atom? and not atom?
+
+(def atomics (fn [lat col]
+	(cond
+		(null? lat) (col () ())
+		(atom? (car lat))
+			(atomics (cdr lat) (fn [atoms lists] (col (cons (car lat) atoms) lists)))
+		:else
+			(atomics (cdr lat) (fn [atoms lists] (col atoms (cons (car lat) lists))))
+	)
+))
+
+; (atomics '() list)
+; (atomics '(1 2) list)
+; (atomics '(1 (2) 3) list)
+
+; Reverse a list, CPS possible?
 
